61051429824ba239735faf5529ec1decb1a827f3
n4beel <nabeel3353045805@hotmail.com>
2023-11-23 18:40:07 +0500

    updated env

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 
 function App() {
   const handleTikTokLogin = () => {
-    const tikTokAuthUrl = `https://www.tiktok.com/v2/auth/authorize?client_key=aw9tg7ty0ju3truy&scope=user.info.basic,video.list&response_type=code&redirect_uri=https%3A%2F%2Ftiktok-auth-poc.web.app%2F&state=fngwwh5076e`;
+    const tikTokAuthUrl = `https://www.tiktok.com/v2/auth/authorize?client_key=${process.env.REACT_APP_CLIENT_KEY}&scope=user.info.basic,video.list&response_type=code&redirect_uri=https%3A%2F%2Ftiktok-auth-poc.web.app%2F&state=fngwwh5076e`;
     window.location.href = tikTokAuthUrl;
   };
 
