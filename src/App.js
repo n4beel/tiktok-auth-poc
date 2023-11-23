f49221d41ee1e2f183b15bc204e4502800f5fb69
n4beel <nabeel3353045805@hotmail.com>
2023-11-23 18:37:09 +0500

    init

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const handleTikTokLogin = () => {
+    const tikTokAuthUrl = `https://www.tiktok.com/v2/auth/authorize?client_key=aw9tg7ty0ju3truy&scope=user.info.basic,video.list&response_type=code&redirect_uri=https%3A%2F%2Ftiktok-auth-poc.web.app%2F&state=fngwwh5076e`;
+    window.location.href = tikTokAuthUrl;
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={handleTikTokLogin}>Login with tiktok</button>
       </header>
     </div>
   );
